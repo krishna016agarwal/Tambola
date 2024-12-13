048d4724d6e80706706a1090da410cb9759e6fe7
Krishna Agarwal <krishna016agrawal@gmail.com>
2024-12-13 08:30:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+v=(h)=>{
+        console.log(h)
+       bb=document.querySelector(`#a${h}`);
+       bb.classList.add("ji")
 
+}
+hi=document.querySelector(".pannel");
+
+s=document.querySelector("#pp")
+s.addEventListener("click",()=>{
+  location.reload()})
+  y=document.querySelector("#jk");
+ 
+l=[];
+ function make(){
+  m=document.querySelector(".jjk");
+  for(let i=1;i<=90;i++){
+    l.push(i);
+  
+    a=document.createElement("div");
+    a.classList.add("jj");
+    a.innerHTML=i;
+    a.setAttribute("id","a"+i);
+    m.append(a);
+    
+  }
+  
